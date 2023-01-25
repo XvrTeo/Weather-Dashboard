b864c41b9d64c3a7a5f6db7939bf7e8d1e3ac35f
Xavier Teo <xvrteo@gmail.com>
2023-01-25 10:27:17 -0800

    Update CSS and JS files to set local storage functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,3 +54,109 @@ var getCurrentConditions = (event) => {
             $('#current-weather').html(currentWeatherHTML);
         })
 }
+
+// Display the 5-day forecast to HTML
+var getFiveDayForecast = (event) => {
+    let city = $('#search-city').val();
+
+    let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial" + "&APPID=" + myOpenWeatherAPI;
+
+    // Fetch from API
+    fetch(queryURL)
+        .then(handleErrors)
+        .then((response) => {
+            return response.json();
+        })
+        .then((response) => {
+
+            let fiveDayForecastHTML = `
+        <h2>5-Day Forecast:</h2>
+        <div id="fiveDayForecastUl" class="flex-wrap d-inline-flex">`;
+
+            // Loop over the 5-day forecast
+            for (let i = 0; i < response.list.length; i++) {
+                let dayData = response.list[i];
+                let dayTimeUTC = dayData.dt;
+                let timeZoneOffset = response.city.timezone;
+                let timeZoneOffsetHours = timeZoneOffset / 60 / 60;
+                let thisMoment = moment.unix(dayTimeUTC).utc().utcOffset(timeZoneOffsetHours);
+                let iconURL = "https://openweathermap.org/img/w/" + dayData.weather[0].icon + ".png";
+
+                if (thisMoment.format("HH:mm:ss") === "11:00:00" || thisMoment.format("HH:mm:ss") === "12:00:00" || thisMoment.format("HH:mm:ss") === "13:00:00") {
+                    fiveDayForecastHTML += `
+                <div class="weather-card card m-2 p0">
+                    <ul class="list-unstyled p-3">
+                        <li>${thisMoment.format("MM/DD/YY")}</li>
+                        <li class="weather-icon"><img src="${iconURL}"></li>
+                        <li>Temp: ${dayData.main.temp}&#8457;</li>
+                        <br>
+                        <li>Wind: ${dayData.wind.speed} MPH</li>
+                        <br>
+                        <li>Humidity: ${dayData.main.humidity}%</li>
+                    </ul>
+                </div>`;
+                }
+            }
+            fiveDayForecastHTML += `</div>`;
+            // Append 5-day forecast to DOM
+            $('#five-day-forecast').html(fiveDayForecastHTML);
+        })
+}
+
+// Save city to localStorage
+var saveCity = (newCity) => {
+    let cityExists = false;
+
+    for (let i = 0; i < localStorage.length; i++) {
+        if (localStorage["cities" + i] === newCity) {
+            cityExists = true;
+            break;
+        }
+    }
+
+    if (cityExists === false) {
+        localStorage.setItem('cities' + localStorage.length, newCity);
+    }
+}
+
+// Show list of previously entered cities
+var renderCities = () => {
+    $('#city-results').empty();
+    // If localStorage is empty
+    if (localStorage.length === 0) {
+        if (lastCity) {
+            $('#search-city').attr("value", lastCity);
+        }
+    } else {
+
+        let lastCityKey = "cities" + (localStorage.length - 1);
+        lastCity = localStorage.getItem(lastCityKey);
+
+        $('#search-city').attr("value", lastCity);
+
+        // Append stored cities to page
+        for (let i = 0; i < localStorage.length; i++) {
+            let city = localStorage.getItem("cities" + i);
+            let cityEl;
+
+            if (currentCity === "") {
+                currentCity = lastCity;
+            }
+
+            if (city === currentCity) {
+                cityEl = `<button type="button" class="list-group-item list-group-item-action active">${city}</button></li>`;
+            } else {
+                cityEl = `<button type="button" class="list-group-item list-group-item-action">${city}</button></li>`;
+            }
+
+            $('#city-results').prepend(cityEl);
+        }
+        // If there is a list of cities, add "clear" button
+        if (localStorage.length > 0) {
+            $('#clear-storage').html($('<a id="clear-storage" href="#">clear</a>'));
+        } else {
+            $('#clear-storage').html('');
+        }
+    }
+
+}
